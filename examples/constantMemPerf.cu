6e18ecc6ef004faf893ae2c7a0de73e3572e5e0b
znan <znan@ncsu.edu>
2018-07-31 15:24:08 -0700

    more test

--- examples/constantMemPerf.cu
+++ examples/constantMemPerf.cu
@@ -105,7 +105,7 @@ int main(int, char**)
   for(int i=0; i<run_times; i++)
   {
     t1 = std::chrono::high_resolution_clock::now();
-    add_kernel<<<2, 256>>>(d_a, d_b, d_sum, size);
+    add_kernel<<<8, 1024>>>(d_a, d_b, d_sum, size);
     t2 = std::chrono::high_resolution_clock::now();
     timing_device += std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
     check_error();
@@ -128,7 +128,7 @@ int main(int, char**)
   for(int i=0; i<run_times; i++)
   {
     t1 = std::chrono::high_resolution_clock::now();
-    add_constant_kernel<<<2, 256>>>(d_a, d_d, d_sum, size);
+    add_constant_kernel<<<8, 1024>>>(d_a, d_d, d_sum, size);
     t2 = std::chrono::high_resolution_clock::now();
     timing_const += std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
     check_error();
